abdf8574446452c8fe6f1e1dd4e3af9091fd5910
Kipp AD <kippad@protonmail.com>
2022-02-05 11:37:16 +0000

    Add difficulty selection page

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,9 +20,9 @@ function enterUsername() {
   if (!username) {
     gameArea.innerHTML = `
       <div id="username-div">
-        <label for="username" class="setup-label">Please enter a Username:</label><br>
+        <label for="username">Please enter a Username:</label><br>
         <input id="username" type="text" placeholder="Enter your username here..." required>
-        <button type="submit" id="submit" class="btn">Submit <i class="fas fa-arrow-right"></i></button>
+        <button type="submit" id="submit" class="submit btn">Submit <i class="fas fa-arrow-right"></i></button>
       </div>
     `;
     document.getElementById('submit').addEventListener('click', storeUsername);
@@ -30,6 +30,38 @@ function enterUsername() {
     selectDifficulty();
   }; 
 };
+
+/**
+* Shows html area where user selects a difficulty
+*/
+function selectDifficulty() {
+  gameArea.innerHTML = `
+    <div class="username-container">
+      <h2>Welcome ${username}! Please select a game difficulty:</h2>
+      <span id="clear-username">Not You?<i class="fas fa-times"></i></span>
+    </div>
+
+    <div class="diff-container">
+      <input type="radio" id="easyDifficulty" name="difficulty" value="easy" checked>
+      <label class="btn" for="easyDifficulty">Easy</label>
+        
+      <input type="radio" id="normalDifficulty" name="difficulty" value="normal">  
+      <label id="normalLabel" class="btn" for="normalDifficulty">Normal <span class="lockIcon"><i class="fas fa-lock"></i></span></label>
+      
+      <input type="radio" id="hardDifficulty" name="difficulty" value="hard">
+      <label id="hardLabel" class="btn" for="hardDifficulty">Hard <span class="lockIcon"><i class="fas fa-lock"></i></span></label>
+    </div>
+    <button type="submit" id="play-btn" class="btn">Play <i class="fas fa-arrow-right"></i></button>
+  `;
+  // 'Not you?' text that clears local storage and reloads enter username page
+  document.getElementById('clear-username').addEventListener('click', clearLocalStorage);
+  // Submit button to load game area with selected difficulty
+  document.getElementById('play-btn').addEventListener('click', runGame);
+};
+
+function runGame() {
+  gameArea.innerHTML = ``;
+}
 
 /** Stores username in local storage upon submission
  */
@@ -41,9 +73,10 @@ function storeUsername() {
   selectDifficulty();
 }
 
-/**
-* Shows html area where user selects a difficulty
-*/
-function selectDifficulty() {
-  gameArea.innerHTML = `test`
+/** Clears storage and assigns username to falsey value form local storage */
+function clearLocalStorage() {
+  localStorage.clear();
+  username = localStorage.getItem('username');
+  enterUsername();
 }
+
