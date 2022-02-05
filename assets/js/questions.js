496dc5db65ce51bbc7e4cd4b7a4810f48ed92c89
Kipp AD <kippad@protonmail.com>
2022-02-05 12:52:32 +0000

    Add question arrays to questions.js and link script to document

---
+++ assets/js/questions.js
@@ -0,0 +1,985 @@
+let easyQuestions = [{
+    question: "Germany",
+    answer: "Berlin",
+  },
+  {
+    question: "Ireland",
+    answer: "Dublin",
+  },
+  {
+    question: "France",
+    answer: "Paris",
+  },
+  {
+    question: "China",
+    answer: "Beijing",
+  },
+  {
+    question: "USA",
+    answer: "Washington DC",
+  },
+  {
+    question: "England",
+    answer: "London",
+  },
+  {
+    question: "Belgium",
+    answer: "Brussels",
+  },
+  {
+    question: "India",
+    answer: "New Delhi",
+  },
+  {
+    question: "Spain",
+    answer: "Madrid",
+  },
+  {
+    question: "Poland",
+    answer: "Warsaw",
+  },
+  {
+    question: "Scotland",
+    answer: "Edinburgh",
+  },
+  {
+    question: "Wales",
+    answer: "Cardiff",
+  },
+  {
+    question: "Italy",
+    answer: "Rome",
+  },
+  {
+    question: "Netherlands",
+    answer: "Amsterdam",
+  },
+  {
+    question: "Sweden",
+    answer: "Stockholm",
+  },
+  {
+    question: "Denmark",
+    answer: "Copenhagen",
+  },
+  {
+    question: "Norway",
+    answer: "Oslo",
+  },
+  {
+    question: "Russia",
+    answer: "Moscow",
+  },
+  {
+    question: "Ukraine",
+    answer: "Kiev",
+  },
+  {
+    question: "Hungary",
+    answer: "Budapest",
+  },
+  {
+    question: "United Arab Emirates",
+    answer: "Abu Dhabi",
+  },
+  {
+    question: "Algeria",
+    answer: "Algiers",
+  },
+  {
+    question: "Andorra",
+    answer: "Andorra la Vella",
+  },
+  {
+    question: "Turkey",
+    answer: "Ankara",
+  },
+  {
+    question: "Paraguay",
+    answer: "Asunción",
+  },
+  {
+    question: "Greece",
+    answer: "Athens",
+  },
+  {
+    question: "Iraq",
+    answer: "Baghdad",
+  },
+  {
+    question: "Azerbaijan",
+    answer: "Baku",
+  },
+  {
+    question: "Thailand",
+    answer: "Bangkok",
+  },
+  {
+    question: "Lebanon",
+    answer: "Beirut",
+  },
+  {
+    question: "Serbia",
+    answer: "Belgrade",
+  },
+  {
+    question: "Switzerland",
+    answer: "Bern",
+  },
+  {
+    question: "South Africa",
+    answer: "Cape Town",
+  },
+  {
+    question: "Brazil",
+    answer: "Brasilia",
+  },
+  {
+    question: "Belgium",
+    answer: "Brussels",
+  },
+  {
+    question: "Egypt",
+    answer: "Cairo",
+  },
+  {
+    question: "Australia",
+    answer: "Canberra",
+  },
+  {
+    question: "Denmark",
+    answer: "Copenhagen",
+  },
+  {
+    question: "Senegal",
+    answer: "Dakar",
+  },
+  {
+    question: "Djibouti",
+    answer: "Djibouti",
+  },
+  {
+    question: "Qatar",
+    answer: "Doha",
+  },
+  {
+    question: "Ireland",
+    answer: "Dublin",
+  },
+  {
+    question: "Gibraltar",
+    answer: "Gibraltar",
+  },
+  {
+    question: "Guatemala",
+    answer: "Guatemala City",
+  },
+  {
+    question: "Vietnam",
+    answer: "Hanoi",
+  },
+  {
+    question: "Cuba",
+    answer: "Havana",
+  },
+  {
+    question: "Finland",
+    answer: "Helsinki",
+  },
+  {
+    question: "Kuwait",
+    answer: "Kuwait City",
+  },
+  {
+    question: "Portugal",
+    answer: "Lisbon",
+  },
+  {
+    question: "Luxembourg",
+    answer: "Luxembourg",
+  },
+  {
+    question: "Mexico",
+    answer: "Mexico City",
+  },
+  {
+    question: "Monaco",
+    answer: "Monaco",
+  },
+  {
+    question: "Panama",
+    answer: "Panama City",
+  },
+  {
+    question: "Czech Republic",
+    answer: "Prague",
+  },
+  {
+    question: "San Marino",
+    answer: "San Marino",
+  },
+  {
+    question: "El Salvador",
+    answer: "San Salvador",
+  },
+  {
+    question: "Singapore",
+    answer: "Singapore",
+  },
+  {
+    question: "Japan",
+    answer: "Tokyo",
+  },
+  {
+    question: "Tunisia",
+    answer: "Tunis",
+  },
+  {
+    question: "Vatican City",
+    answer: "Vatican City",
+  },
+  {
+    question: "Austria",
+    answer: "Vienna",
+  }
+];
+
+let normalQuestions = [{
+    question: "Croatia",
+    answer: "Zagreb",
+  },
+  {
+    question: "Canada",
+    answer: "Ottawa",
+  },
+  {
+    question: "Bulgaria",
+    answer: "Sofia",
+  },
+  {
+    question: "Romania",
+    answer: "Bucharest",
+  },
+  {
+    question: "Argentina",
+    answer: "Buenos Aires",
+  },
+  {
+    question: "Chile",
+    answer: "Santiago",
+  },
+  {
+    question: "Egypt",
+    answer: "Cairo",
+  },
+  {
+    question: "Rwanda",
+    answer: "Kigali",
+  },
+  {
+    question: "Senegal",
+    answer: "Dakar",
+  },
+  {
+    question: "New Zealand",
+    answer: "Wellington",
+  },
+  {
+    question: "Australia",
+    answer: "Canberra",
+  },
+  {
+    question: "Syria",
+    answer: "Damascus",
+  },
+  {
+    question: "Azerbaijan",
+    answer: "Baku",
+  },
+  {
+    question: "Slovakia",
+    answer: "Bratislava",
+  },
+  {
+    question: "Belarus",
+    answer: "Minsk",
+  },
+  {
+    question: "Afghanistan",
+    answer: "Kabul"
+  },
+  {
+    question: "Ghana",
+    answer: "Accra",
+  },
+  {
+    question: "Jordan",
+    answer: "Amman",
+  },
+  {
+    question: "Turkmenistan",
+    answer: "Ashgabat",
+  },
+  {
+    question: "Mali",
+    answer: "Bamako",
+  },
+  {
+    question: "Guinea-Bissau",
+    answer: "Bissau",
+  },
+  {
+    question: "Colombia",
+    answer: "Bogota",
+  },
+  {
+    question: "Venezuala",
+    answer: "Caracas",
+  },
+  {
+    question: "Moldova",
+    answer: "Chișinău",
+  },
+  {
+    question: "Sri Lanka",
+    answer: "Sri Jayawardenepura Kotte",
+  },
+  {
+    question: "Bangladesh",
+    answer: "Dhaka",
+  },
+  {
+    question: "Dili",
+    answer: "East Timor",
+  },
+  {
+    question: "Cayman Islands",
+    answer: "George Town",
+  },
+  {
+    question: "Guyana",
+    answer: "Georgetown",
+  },
+  {
+    question: "Bermuda",
+    answer: "Hamilton",
+  },
+  {
+    question: "Pakistan",
+    answer: "Islamabad",
+  },
+  {
+    question: "Indonesia",
+    answer: "Jakarta",
+  },
+  {
+    question: "Uganda",
+    answer: "Kampala",
+  },
+  {
+    question: "Nepal",
+    answer: "Kathmandu",
+  },
+  {
+    question: "Jamaica",
+    answer: "Kingston",
+  },
+  {
+    question: "Malaysia",
+    answer: "Kuala Lumpur",
+  },
+  {
+    question: "Bolivia",
+    answer: "La Paz, Sucre",
+  },
+  {
+    question: "Peru",
+    answer: "Lima",
+  },
+  {
+    question: "Slovenia",
+    answer: "Ljubljana",
+  },
+  {
+    question: "Zambia",
+    answer: "Lusaka",
+  },
+  {
+    question: "Nicaragua",
+    answer: "Managua",
+  },
+  {
+    question: "Bahrain",
+    answer: "Manama",
+  },
+  {
+    question: "Phillipines",
+    answer: "Manila",
+  },
+  {
+    question: "Mozambique",
+    answer: "Maputo",
+  },
+  {
+    question: "Liberia",
+    answer: "Monrovia",
+  },
+  {
+    question: "Uruguay",
+    answer: "Montevideo",
+  },
+  {
+    question: "Oman",
+    answer: "Muscat",
+  },
+  {
+    question: "Kenya",
+    answer: "Nairobi",
+  },
+  {
+    question: "Bahamas",
+    answer: "Nassau",
+  },
+  {
+    question: "Cyprus",
+    answer: "Nicosia",
+  },
+  {
+    question: "Greenland",
+    answer: "Nuuk",
+  },
+  {
+    question: "Cambodia",
+    answer: "Phnom Penh",
+  },
+  {
+    question: "Trinidad and Tobago",
+    answer: "Port of Spain",
+  },
+  {
+    question: "Haiti",
+    answer: "Port-au-Prince",
+  },
+  {
+    question: "Cape Verde",
+    answer: "Praia",
+  },
+  {
+    question: "Kosovo",
+    answer: "Pristina",
+  },
+  {
+    question: "North Korea",
+    answer: "Pyongyang",
+  },
+  {
+    question: "Ecuador",
+    answer: "Quito",
+  },
+  {
+    question: "Morocco",
+    answer: "Rabat",
+  },
+  {
+    question: "Iceland",
+    answer: "Reykjavík",
+  },
+  {
+    question: "Latvia",
+    answer: "Riga",
+  },
+  {
+    question: "Saudi Arabia",
+    answer: "Riyadh",
+  },
+  {
+    question: "Costa Rica",
+    answer: "San José",
+  },
+  {
+    question: "Puerto Rico",
+    answer: "San Juan",
+  },
+  {
+    question: "Dominican Republic",
+    answer: "Santo Domingo",
+  },
+  {
+    question: "Bosnia and Herzegovina",
+    answer: "Sarajevo",
+  },
+  {
+    question: "South Korea",
+    answer: "Seoul",
+  },
+  {
+    question: "North Macedonia",
+    answer: "Skopje",
+  },
+  {
+    question: "Jersey",
+    answer: "St. Helier",
+  },
+  {
+    question: "Antigua and Barbuda",
+    answer: "St. John's",
+  },
+  {
+    question: "Taiwan",
+    answer: "Taipei",
+  },
+  {
+    question: "Iran",
+    answer: "Tehran",
+  },
+  {
+    question: "Faroe Islands",
+    answer: "Tórshavn",
+  },
+  {
+    question: "Libya",
+    answer: "Tripoli",
+  },
+  {
+    question: "Malta",
+    answer: "Valletta",
+  },
+  {
+    question: "Seychelles",
+    answer: "Victoria",
+  },
+  {
+    question: "Lithuania",
+    answer: "Vilnius",
+  },
+  {
+    question: "Namibia",
+    answer: "Windhoek",
+  }
+];
+
+let hardQuestions = [{
+    question: "Ivory Coast",
+    answer: "Yamoussoukro",
+  },
+  {
+    question: "Nigeria",
+    answer: "Abuja",
+  },
+  {
+    question: "Yemen",
+    answer: "Aden",
+  },
+  {
+    question: "Niue",
+    answer: "Alofi",
+  },
+  {
+    question: "Pitcairn Islands",
+    answer: "Adamstown",
+  },
+  {
+    question: "Madagascar",
+    answer: "Antananarivo",
+  },
+  {
+    question: "Samoa",
+    answer: "Apia",
+  },
+  {
+    question: "Eritrea",
+    answer: "Asmara",
+  },
+  {
+    question: "Cook Islands",
+    answer: "Avarua",
+  },
+  {
+    question: "Brunei",
+    answer: "Bandar Seri Begawan",
+  },
+  {
+    question: "Central African Republic",
+    answer: "Bangui",
+  },
+  {
+    question: "The Gambia",
+    answer: "Banjul",
+  },
+  {
+    question: "Saint Kitts and Nevis",
+    answer: "Basseterre",
+  },
+  {
+    question: "Belize",
+    answer: "Belmopan",
+  },
+  {
+    question: "Kyrgyzstan",
+    answer: "Bishkek",
+  },
+  {
+    question: "Montserrat",
+    answer: "Plymouth",
+  },
+  {
+    question: "Congo",
+    answer: "Brazzaville",
+  },
+  {
+    question: "Barbados",
+    answer: "Bridgetown",
+  },
+  {
+    question: "Ethiopa",
+    answer: "Addis Ababa",
+  },
+  {
+    question: "Saint Lucia",
+    answer: "Castries",
+  },
+  {
+    question: "Montenegro",
+    answer: "Podgorica",
+  },
+  {
+    question: "U.S. Virgin Islands",
+    answer: "Charlotte Amalie",
+  },
+  {
+    question: "Turks and Caicos Islands",
+    answer: "Cockburn Town",
+  },
+  {
+    question: "Guinea",
+    answer: "Conakry",
+  },
+  {
+    question: "Benin",
+    answer: "Porto-Novo",
+  },
+  {
+    question: "Tanzania",
+    answer: "Dodoma",
+  },
+  {
+    question: "Isle of Man",
+    answer: "Douglas",
+  },
+  {
+    question: "Tajikistan",
+    answer: "Dushanbe",
+  },
+  {
+    question: "Western Sahara",
+    answer: "El Aaiún",
+  },
+  {
+    question: "Christmas Island",
+    answer: "Flying Fish Cove",
+  },
+  {
+    question: "Sierra Leone",
+    answer: "Freetown",
+  },
+  {
+    question: "Tuvalu",
+    answer: "Funafuti",
+  },
+  {
+    question: "Botswana",
+    answer: "Gaborone",
+  },
+  {
+    question: "Ascension Island",
+    answer: "Georgetown",
+  },
+  {
+    question: "Burundi",
+    answer: "Gitega",
+  },
+  {
+    question: "Saint Barthélemy",
+    answer: "Gustavia",
+  },
+  {
+    question: "Guam",
+    answer: "Hagåtña",
+  },
+  {
+    question: "Zimbabwe",
+    answer: "Harare",
+  },
+  {
+    question: "Somaliland",
+    answer: "Hargeisa",
+  },
+  {
+    question: "Solomon Islands",
+    answer: "Honiara",
+  },
+  {
+    question: "Saint-Helena",
+    answer: "Jamestown",
+  },
+  {
+    question: "South Sudan",
+    answer: "Juba",
+  },
+  {
+    question: "Sudan",
+    answer: "Khartoum",
+  },
+  {
+    question: "South Georgia and the South Sandwich Islands",
+    answer: "King Edward Point",
+  },
+  {
+    question: "Norfolk Island",
+    answer: "Kingston",
+  },
+  {
+    question: "Saint Vincent and the Grenadines",
+    answer: "Kingstown",
+  },
+  {
+    question: "DR Congo",
+    answer: "Kinshasa",
+  },
+  {
+    question: "Gabon",
+    answer: "Libreville",
+  },
+  {
+    question: "Malawi",
+    answer: "Lilongwe",
+  },
+  {
+    question: "Eswatini",
+    answer: "Lobamba",
+  },
+  {
+    question: "Togo",
+    answer: "Lomé",
+  },
+  {
+    question: "Angola",
+    answer: "Luanda",
+  },
+  {
+    question: "Marshall Islands",
+    answer: "Majuro",
+  },
+  {
+    question: "Equatorial Guinea",
+    answer: "Malabo",
+  },
+  {
+    question: "Maldives",
+    answer: "Malé",
+  },
+  {
+    question: "Åland",
+    answer: "Mariehamm",
+  },
+  {
+    question: "Saint Martin",
+    answer: "Marigot",
+  },
+  {
+    question: "Lesotho",
+    answer: "Maseru",
+  },
+  {
+    question: "Wallis and Futuna",
+    answer: "Mata Utu",
+  },
+  {
+    question: "Somalia",
+    answer: "Mogadishu",
+  },
+  {
+    question: "Comoros",
+    answer: "Moroni",
+  },
+  {
+    question: "Myanmar",
+    answer: "Naypyidaw",
+  },
+  {
+    question: "Chad",
+    answer: "N'Djamena",
+  },
+  {
+    question: "Palau",
+    answer: "Ngerulmud",
+  },
+  {
+    question: "Niger",
+    answer: "Niamey",
+  },
+  {
+    question: "Mauritania",
+    answer: "Nouakchott",
+  },
+  {
+    question: "New Caledonia",
+    answer: "Nouméa",
+  },
+  {
+    question: "Tonga",
+    answer: "Nukuʻalofa",
+  },
+  {
+    question: "Kazakhstan",
+    answer: "Nur-Sultan",
+  },
+  {
+    question: "Aruba",
+    answer: "Oranjestad",
+  },
+  {
+    question: "Burkina Faso",
+    answer: "Ouagadougou",
+  },
+  {
+    question: "American Samoa",
+    answer: "Pago Pago",
+  },
+  {
+    question: "Micronesia",
+    answer: "Palikir",
+  },
+  {
+    question: "French Polynesia",
+    answer: "Papeete",
+  },
+  {
+    question: "Suriname",
+    answer: "Paramaribo",
+  },
+  {
+    question: "Sint Maarten",
+    answer: "Philipsburg",
+  },
+  {
+    question: "Mauritius",
+    answer: "Port Louis",
+  },
+  {
+    question: "Papua New Guinea",
+    answer: "Port Moresby",
+  },
+  {
+    question: "Vanuatu",
+    answer: "Port Vila",
+  },
+  {
+    question: "British Virgin Islands",
+    answer: "Road Town",
+  },
+  {
+    question: "Dominica",
+    answer: "Roseau",
+  },
+  {
+    question: "Northern Mariana Islands",
+    answer: "Saipan",
+  },
+  {
+    question: "São Tomé and Príncipe",
+    answer: "São Tomé",
+  },
+  {
+    question: "Kiribati",
+    answer: "South Tarawa",
+  },
+  {
+    question: "Grenada",
+    answer: "St George's",
+  },
+  {
+    question: "Guernsey",
+    answer: "St. Peter Port",
+  },
+  {
+    question: "Saint Pierre and Miquelon",
+    answer: "St. Pierre",
+  },
+  {
+    question: "Falkland Islands",
+    answer: "Stanley",
+  },
+  {
+    question: "Republic of Artsakh",
+    answer: "Stepanakert",
+  },
+  {
+    question: "Abkhazia",
+    answer: "Sukhumi",
+  },
+  {
+    question: "Fiji",
+    answer: "Suva",
+  },
+  {
+    question: "Uzbekistan",
+    answer: "Tashkent",
+  },
+  {
+    question: "Georgia",
+    answer: "Tbilisi",
+  },
+  {
+    question: "Honduras",
+    answer: "Tegucigalpa",
+  },
+  {
+    question: "Bhutan",
+    answer: "Thimphu",
+  },
+  {
+    question: "Transnistria",
+    answer: "Tiraspol",
+  },
+  {
+    question: "South Ossetia",
+    answer: "Tskhinvali",
+  },
+  {
+    question: "Mongolia",
+    answer: "Ulaanbaatar",
+  },
+  {
+    question: "Liechtenstein",
+    answer: "Vaduz",
+  },
+  {
+    question: "Anguilla",
+    answer: "The Valley",
+  },
+  {
+    question: "Laos",
+    answer: "Vientiane",
+  },
+  {
+    question: "Cocos (Keeling) Islands",
+    answer: "West Island",
+  },
+  {
+    question: "Curaçao",
+    answer: "Willemstad",
+  },
+  {
+    question: "Cameroon",
+    answer: "Yaoundé",
+  },
+  {
+    question: "Nauru",
+    answer: "Yaren",
+  },
+  {
+    question: "Armenia",
+    answer: "Yerevan",
+  }
+];
