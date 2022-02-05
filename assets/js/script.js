490083fae2b66e778df74a1fbd53f927f7122113
Kipp AD <kippad@protonmail.com>
2022-02-05 14:57:18 +0000

    Add next question function so that new question is loaded every time user selects an answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,17 @@ playBtn.addEventListener('click', enterUsername);
 
 const gameArea = document.getElementById('game-area');
 
-// Empty variables for later reassignment
+let answerButtons = document.getElementsByClassName('answer-btn');
+
+
+// General variables
 let username = localStorage.getItem('username');
 let difficulty;
 let questionDifficulty;
+let question;
+let answer;
+let scoreCounter = 0;
+let questionCounter = 1;
 
 /** Takes user to input where username is stored in local storage */
 function enterUsername() {
@@ -67,14 +74,14 @@ function setQuestionDifficulty() {
   } else if (difficultySelection === "hard") {
     questionDifficulty = [...hardQuestions];
   };
-
+  let i = 0;
   runGame();
 };
 
 function runGame() {
   gameArea.innerHTML = `
     <h2>Good luck ${username}, this is question <span id="questionCounter"></span> of 12</h2>
-    <p>Which of the following cities is the capital of <span id="questionSub"></span>?</p>
+    <p>Which of the following cities is the capital of <span id="question-sub"></span>?</p>
     <div class="answer-container">  
       <button class="answer-btn btn">Answer1</button>
       <button class="answer-btn btn">Answer2</button>
@@ -84,7 +91,51 @@ function runGame() {
     <p>Current Score: <span id="scoreCounter"></span></p>
     <p>High Score: <span id="highScoreCounter">0</span></p>
   `;
+  i = 0;
+  nextQuestion();
+  checkAnswer();
+};
+
+/** Sets all counters and ensures game area populated with question */
+function nextQuestion() {
+  if (questionCounter <= 12) {
+    populateQuestion();
+    questionCounter += 1;
+    i++;
+  }
+};
+
+/** Sets the html substitution values in game area */
+function populateQuestion() {
+  // Populates answer buttons with random answer from array
+  answerButtons = Array.from(answerButtons);
+  for (answerButton of answerButtons) {
+    answerButton.innerHTML = questionDifficulty[Math.floor(Math.random()* questionDifficulty.length)].answer;
+  };
+  question = questionDifficulty[i].question;
+  answer = questionDifficulty[i].answer;
+  // Replaces inner html of random answer box with correct answer
+  answerButtons[Math.floor(Math.random() * 4)].innerHTML = answer;
+
+  document.getElementById('question-sub').innerHTML = question;
+  document.getElementById('questionCounter').innerHTML = questionCounter;
+  document.getElementById('scoreCounter').innerHTML = scoreCounter;
 }
+
+function checkAnswer() {
+  answerButtons.forEach(answerButton => {
+    answerButton.addEventListener('click', function () {
+      if (answerButton.innerHTML === answer) {
+        alert('correct');
+        scoreCounter +=1;
+        nextQuestion();
+      } else {
+        alert('incorrect');
+        nextQuestion();
+      };
+    });
+  });
+};
 
 /** Checks form validity and stores username in local storage upon submission */
 function storeUsername() {
@@ -94,7 +145,8 @@ function storeUsername() {
     localStorage.setItem('username', username);
     console.log(localStorage);
     selectDifficulty();
-  } 
+  }; 
+  return username;
 };
 
 /** Clears storage and assigns username to falsey value form local storage */
@@ -102,5 +154,5 @@ function clearLocalStorage() {
   localStorage.clear();
   username = localStorage.getItem('username');
   enterUsername();
-}
+};
 
